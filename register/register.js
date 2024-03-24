c85df108ffce84cb7e0270019b671a6c563caca8
JimWould99 <130195351+JimWould99@users.noreply.github.com>
2024-03-24 17:49:47 +0000

    comments

--- register/register.js
+++ register/register.js
@@ -1,68 +1,86 @@
-const username_span = document.querySelector('#span_username')
-const password_span = document.querySelector('#span_password')
+/*
+below section:
+Connects javascript variables to the dom for manipulation.
+Establishes connections to each section of the form
+as well as the user inputs and feedback sections.
 
-const username = document.querySelector('#username')
-const password = document.querySelector('#password')
+*/
+const username_span = document.querySelector("#span_username");
+const password_span = document.querySelector("#span_password");
 
-const username_section = document.querySelector('#username_section')
-const password_section = document.querySelector('#password_section')
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
 
-const form = document.querySelector('form')
+const username_section = document.querySelector("#username_section");
+const password_section = document.querySelector("#password_section");
 
-form.addEventListener('submit', (e) => {
-     e.preventDefault()
-     
-     username_section.removeAttribute("id", "invalid")
-     password_section.removeAttribute("id", "invalid")
+const form = document.querySelector("form");
 
-     username_list = []
-     password_list = []
+form.addEventListener("submit", (e) => {
+  //reset feedback for corrections after first/second submission
+  username_section.removeAttribute("id", "invalid");
+  password_section.removeAttribute("id", "invalid");
 
-     if (username.value == "" || username.value == null){
-        username_list.push("Fill in username/ email")
-        username_section.setAttribute("id", "invalid")
-     } else if (username.value.includes('@')){
-        console.log('includes @')
-        if (!username.value.match(/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/)){
-            username_list.push("Not a valid email")
-            username_section.setAttribute("id", "invalid")
-        }
-     } else{
-        if (username.value.length <= 6) {
-            username_list.push("username minimum 7 characters")
-            username_section.setAttribute("id", "invalid")
-         }
-     }
+  // create lists in case there are multiple feedback comments
+  // these will be added to the spans at the end
 
+  username_list = [];
+  password_list = [];
 
-     if (password.value == "" || password.value == null){
-        password_list.push("Fill in password")
-        password_section.setAttribute("id", "invalid")
-    } else {
-        
-       const numbers = /[0-9]/g
+  // check whether the input is filled in and if it is a username or email
+  // then check whether the following input is valid
 
-       if (password.value.length <= 7) {
-        password_list.push('minmum 8 characters')
-        password_section.setAttribute("id", "invalid")
-       }
+  if (username.value == "" || username.value == null) {
+    username_list.push("Fill in username/ email");
+    username_section.setAttribute("id", "invalid");
+  } else if (username.value.includes("@")) {
+    if (
+      !username.value.match(/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/)
+    ) {
+      username_list.push("Not a valid email");
+      username_section.setAttribute("id", "invalid");
+    }
+  } else {
+    if (username.value.length <= 6) {
+      username_list.push("username minimum 7 characters");
+      username_section.setAttribute("id", "invalid");
+    }
+  }
 
-       if (!password.value.match(numbers)){
-        password_list.push("minimum one number")
-        password_section.setAttribute("id", "invalid")
-       }
+  // check password has been filled or valid
+  if (password.value == "" || password.value == null) {
+    password_list.push("Fill in password");
+    password_section.setAttribute("id", "invalid");
+  } else {
+    const numbers = /[0-9]/g;
+
+    if (password.value.length <= 7) {
+      password_list.push("minmum 8 characters");
+      password_section.setAttribute("id", "invalid");
     }
 
-     username_span.textContent = username_list
-     password_span.textContent = password_list
+    if (!password.value.match(numbers)) {
+      password_list.push("minimum one number");
+      password_section.setAttribute("id", "invalid");
+    }
+  }
 
-     if (username_list.length == 0){
-        username_section.setAttribute('id', 'valid')
-        username_span.textContent = "valid username"
-     } 
-     if (password_list.length == 0){
-         password_section.setAttribute('id', 'valid')
-         password_span.textContent = "valid password"
-     }
+  username_span.textContent = username_list;
+  password_span.textContent = password_list;
 
-})
+  //give user feedback for which inputs are valid
+  // also decide if to send the form
+
+  if (username_list.length == 0) {
+    username_section.setAttribute("id", "valid");
+    username_span.textContent = "valid username";
+  } else {
+    e.preventDefault();
+  }
+  if (password_list.length == 0) {
+    password_section.setAttribute("id", "valid");
+    password_span.textContent = "valid password";
+  } else {
+    e.preventDefault();
+  }
+});
--- sign_up/verification.js
+++ sign_up/verification.js
@@ -1,134 +1,154 @@
-const username_span = document.querySelector('#span_username')
-const email_span = document.querySelector('#span_email')
-const password_span = document.querySelector('#span_password')
-const confirm_span= document.querySelector('#span_confirmation')
+/*
+below section:
+Connects javascript variables to the dom for manipulation.
+Establishes connections to each section of the form
+as well as the user inputs and feedback sections.
 
-const username = document.querySelector('#username')
-const email = document.querySelector('#email')
-const password = document.querySelector('#password')
-const confirmation = document.querySelector('#confirm')
+*/
+const username_span = document.querySelector("#span_username");
+const email_span = document.querySelector("#span_email");
+const password_span = document.querySelector("#span_password");
+const confirm_span = document.querySelector("#span_confirmation");
 
-const username_section = document.querySelector('#first_sub')
-const email_section = document.querySelector('#second_sub')
-const password_section = document.querySelector('#third_sub')
-const confirm_section = document.querySelector('#fourth_sub')
+const extra_span = document.querySelector("#radio_match");
 
-const form = document.querySelector('form')
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const confirmation = document.querySelector("#confirm");
 
+const asm_button = document.querySelector("#asm");
+const ir_button = document.querySelector("#ir");
 
-form.addEventListener('submit', (e) => {
-       e.preventDefault()
-       username_section.removeAttribute("id", "invalid")
-       email_section.removeAttribute("id", "invalid")
-       password_section.removeAttribute("id", "invalid")
-       confirm_section.removeAttribute("id", "invalid")
-       
-       username_list = []
-       password_list = []
-       confirmation_list = []
+const username_section = document.querySelector("#first_sub");
+const email_section = document.querySelector("#second_sub");
+const password_section = document.querySelector("#third_sub");
+const confirm_section = document.querySelector("#fourth_sub");
 
-       if (email.value == "" || email.value == null){
-           email_span.textContent = "Fill in email"
-           email_section.setAttribute("id", "invalid")
-       } else if (!email.value.match(/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/)){
-           email_span.textContent = "Invalid email address"
-           email_section.setAttribute("id", "invalid")
-       } else {
-         email_span.textContent = "email valid"
-         email_section.setAttribute('id', 'valid')
-       }
+const extra_section = document.querySelector("#extra_section");
 
-       if (username.value == "" || username.value == null){
-           username_list.push("Fill in username")
-           username_section.setAttribute("id", "invalid")
-       } else if (username.value.length <= 6) {
-          username_list.push("minimum 7 characters")
-          username_section.setAttribute("id", "invalid")
-       }
+const form = document.querySelector("form");
 
+form.addEventListener("submit", (e) => {
+  //reset feedback for corrections after first/second submission
 
-       if (confirmation.value !== password.value) {
-        confirmation_list.push(" Passwords must match")
-        password_list.push(" Passwords must match")
-        password_section.setAttribute("id", "invalid")
-        confirm_section.setAttribute("id", "invalid")
+  username_section.removeAttribute("id", "invalid");
+  email_section.removeAttribute("id", "invalid");
+  password_section.removeAttribute("id", "invalid");
+  confirm_section.removeAttribute("id", "invalid");
+  extra_section.removeAttribute("id", "invalid");
+
+  // create lists in case there are multiple feedback comments
+  // these will be added to the spans at the end
+
+  let username_list = [];
+  let password_list = [];
+  let confirmation_list = [];
+  let extra_list = [];
+
+  // check a radio button has been selected
+  if (!asm_button.checked && !ir_button.checked) {
+    extra_list.push("Check user option");
+    extra_section.setAttribute("id", "invalid");
+  }
+
+  // check email has been filled or valid
+  if (email.value == "" || email.value == null) {
+    email_span.textContent = "Fill in email";
+    email_section.setAttribute("id", "invalid");
+  } else if (
+    !email.value.match(/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/)
+  ) {
+    email_span.textContent = "Invalid email address";
+    email_section.setAttribute("id", "invalid");
+  } else {
+    email_span.textContent = "email valid";
+    email_section.setAttribute("id", "valid");
+  }
+
+  // check username has been filled or valid
+  if (username.value == "" || username.value == null) {
+    username_list.push("Fill in username");
+    username_section.setAttribute("id", "invalid");
+  } else if (username.value.length <= 6) {
+    username_list.push("Min. 7 characters");
+    username_section.setAttribute("id", "invalid");
+  }
+
+  // check passwords match
+  if (confirmation.value !== password.value) {
+    extra_list.push(" Passwords must match");
+    extra_section.setAttribute("id", "invalid");
+  }
+
+  // check confirmation password has been filled or valid
+  if (confirmation.value == "" || confirmation.value == null) {
+    confirmation_list.push("Fill in confirmation");
+    confirm_section.setAttribute("id", "invalid");
+  } else {
+    const numbers = /[0-9]/g;
+
+    if (confirmation.value.length <= 7) {
+      confirmation_list.push(" Min. 8 characters");
+      confirm_section.setAttribute("id", "invalid");
     }
 
-      
-       if (confirmation.value == "" || confirmation.value == null){
-        confirmation_list.push("Fill in confirmation")
-        confirm_section.setAttribute("id", "invalid")
-    } else {
-        const numbers = /[0-9]/g
+    if (!confirmation.value.match(numbers)) {
+      confirmation_list.push(" Min. 1 number");
+      confirm_section.setAttribute("id", "invalid");
+    }
+  }
 
-        if (confirmation.value.length <= 7) {
-        confirmation_list.push("minimum 8 characters")
-        confirm_section.setAttribute("id", "invalid")
-       } 
+  // check password has been filled or valid
+  if (password.value == "" || password.value == null) {
+    password_list.push("Fill in password");
+    password_section.setAttribute("id", "invalid");
+  } else {
+    const numbers = /[0-9]/g;
 
-       if (!confirmation.value.match(numbers)){
-        confirmation_list.push("minimum one number")
-        confirm_section.setAttribute("id", "invalid")
-       }
-
+    if (password.value.length <= 7) {
+      password_list.push(" Min. 8 characters");
+      password_section.setAttribute("id", "invalid");
     }
 
-    if (password.value == "" || password.value == null){
-        password_list.push("Fill in password")
-        password_section.setAttribute("id", "invalid")
-    } else {
-        
-       const numbers = /[0-9]/g
-
-       if (password.value.length <= 7) {
-        password_list.push('Minimum 8 characters')
-        password_section.setAttribute("id", "invalid")
-       }
-
-       if (!password.value.match(numbers)){
-        password_list.push("Minimum one number")
-        password_section.setAttribute("id", "invalid")
-       }
+    if (!password.value.match(numbers)) {
+      password_list.push(" Min. 1 number");
+      password_section.setAttribute("id", "invalid");
     }
+  }
 
-       /*
-       numbers = [0,1,2,3,4,5,6,7,8,9]
-       let has_number = false
-       for (let i = 0; i <= numbers.length-1; i++){
-          if(password.value.includes(numbers[i])){
-              has_number = true;
-          } 
-       }
-       if(!has_number){
-        password_list.push("Minimum one number")
-        password_section.setAttribute("id", "invalid")
-       }*/
+  username_span.textContent = username_list;
+  password_span.textContent = password_list;
+  confirm_span.textContent = confirmation_list;
+  extra_span.textContent = extra_list;
 
-       username_span.textContent = username_list
-       password_span.textContent = password_list
-       confirm_span.textContent = confirmation_list
+  //give user feedback for which inputs are valid
+  // also decide if to send the form
 
-       if (username_list.length == 0){
-          username_section.setAttribute('id', 'valid')
-          username_span.textContent = "valid username"
-       } 
-       if (password_list.length == 0){
-           password_section.setAttribute('id', 'valid')
-           password_span.textContent = "valid password"
-       }
+  if (username_list.length == 0) {
+    username_section.setAttribute("id", "valid");
+    username_span.textContent = "valid username";
+  } else {
+    e.preventDefault();
+  }
+  if (password_list.length == 0) {
+    password_section.setAttribute("id", "valid");
+    password_span.textContent = "valid password";
+  } else {
+    e.preventDefault();
+  }
 
-       if(confirmation_list.length == 0){
-          confirm_section.setAttribute('id', 'valid')
-          confirm_span.textContent = "valid confirmation"
-       }
-       /*
-       password_section.setAttribute("id", "invalid")
-       confirm_section.setAttribute("id", "invalid") */
+  if (confirmation_list.length == 0) {
+    confirm_section.setAttribute("id", "valid");
+    confirm_span.textContent = "valid confirmation";
+  } else {
+    e.preventDefault();
+  }
 
-
-       console.log(`username: ${username.value}`)
-       console.log(`password: ${password.value}`)
-       console.log(`confirm: ${confirm.value}`)
-
-  
-  })
+  if (extra_list.length == 0) {
+    extra_section.setAttribute("id", "valid");
+    extra_span.textContent = "valid confirmation";
+  } else {
+    e.preventDefault();
+  }
+});
