eebdee8c4435dc68704ec80f62507cd171eefc5e
JimWould99 <130195351+JimWould99@users.noreply.github.com>
2024-04-05 17:04:24 +0100

    edited search

--- search/search.js
+++ search/search.js
@@ -1,27 +1,62 @@
 const cards = document.querySelectorAll("#research_card");
-
 for (let i = 0; i <= cards.length - 1; i++) {
   cards[i].style.display = "none";
 }
 
 function checkResults() {
-  const search = document.querySelector("input");
+  const search = document.querySelector("#search_bar");
   const search_value = search.value.toLowerCase();
-
   const authors = document.querySelectorAll("#author");
+  const titles = document.querySelectorAll("#title");
+
+  // let titles = document.querySelectorAll("#title");
+  let author_button = document.querySelector("#author_select");
+  let title_button = document.querySelector("#title_select");
 
   if (search_value === "") {
+    if (author_button.checked) {
+      search.placeholder = "Search by author";
+    } else if (title_button.checked) {
+      search.placeholder = "Search by title";
+    }
     return;
   }
 
-  for (let i = 0; i <= authors.length - 1; i++) {
-    cards[i].style.display = "block";
-
-    if (authors[i].innerHTML.toLowerCase().includes(search_value)) {
-      console.log(`author match: ${authors[i].innerHTML}`);
-      cards[i].style.display = "";
-    } else {
-      cards[i].style.display = "none";
+  if (author_button.checked) {
+    search.placeholder = "Search by authors";
+    console.log("clicked author");
+    for (let i = 0; i <= authors.length - 1; i++) {
+      cards[i].style.display = "block";
+      if (authors[i].innerHTML.toLowerCase().includes(search_value)) {
+        console.log(`author match: ${authors[i].innerHTML}`);
+        cards[i].style.display = "";
+      } else {
+        cards[i].style.display = "none";
+      }
+    }
+  } else if (title_button.checked) {
+    search.placeholder = "Search by title";
+    console.log("clicked title");
+    for (let i = 0; i <= titles.length - 1; i++) {
+      cards[i].style.display = "block";
+      if (titles[i].innerHTML.toLowerCase().includes(search_value)) {
+        console.log(`author match: ${titles[i].innerHTML}`);
+        cards[i].style.display = "";
+      } else {
+        cards[i].style.display = "none";
+      }
+    }
+  } else {
+    for (let i = 0; i <= titles.length - 1; i++) {
+      cards[i].style.display = "block";
+      if (
+        titles[i].innerHTML.toLowerCase().includes(search_value) ||
+        authors[i].innerHTML.toLowerCase().includes(search_value)
+      ) {
+        cards[i].style.display = "";
+      } else {
+        cards[i].style.display = "none";
+      }
     }
   }
 }
