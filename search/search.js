7f509bcad0ee308a6f529ee30cc48a97785052ad
JimWould99 <130195351+JimWould99@users.noreply.github.com>
2024-04-17 12:15:59 +0100

    backup

--- search/search.js
+++ search/search.js
@@ -1,18 +1,20 @@
+/*   
+hide display of all the cards before the user searches
+*/
 const cards = document.querySelectorAll("#research_card");
 for (let i = 0; i <= cards.length - 1; i++) {
   cards[i].style.display = "none";
 }
-
+// function below called upon any changes in search bar
+// Research hence updated each time
 function checkResults() {
   const search = document.querySelector("#search_bar");
   const search_value = search.value.toLowerCase();
   const authors = document.querySelectorAll("#author");
   const titles = document.querySelectorAll("#title");
-
-  // let titles = document.querySelectorAll("#title");
   let author_button = document.querySelector("#author_select");
   let title_button = document.querySelector("#title_select");
-
+  // Check which search parameter is checked to change display
   if (search_value === "") {
     if (author_button.checked) {
       search.placeholder = "Search by author";
@@ -21,7 +23,8 @@ function checkResults() {
     }
     return;
   }
-
+  // Alter research shown based upon option selected
+  // Research that matches is unhidden in display
   if (author_button.checked) {
     search.placeholder = "Search by authors";
     console.log("clicked author");
