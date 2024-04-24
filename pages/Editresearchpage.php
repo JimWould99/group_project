cfbb3d8dc836a20f2cf8ffcf9f1c39c96038b6a7
JimWould99 <130195351+JimWould99@users.noreply.github.com>
2024-04-24 15:39:13 +0100

    edit research

--- pages/Editresearchpage.php
+++ pages/Editresearchpage.php
@@ -47,67 +47,69 @@
 	</head>
 	<body>
 	<?php genHeader($profileId,$accounttype);?>
-		<div id="reject-message">
-		<?php
-			if ($newPage == FALSE ){// change to false
-				if($research["Verified"] == false){
-					if ($research["RejectMessage"] != ""){
-						echo 'Rejected. Reason:'.$research["RejectMessage"];
-					} else{
-						echo 'Not yet Approved/Rejected';
-					}
-				} else{
-					echo 'Approved!';
-				}
-			}
-		 ?>
-		</div>
-		<?php
-			if ($newPage == FALSE){
-				echo '<form id="researchPage" action="../scripts/phpScripts/submitresearch.php?_id='.$_GET["_id"].'" method="POST">';
-			} else {
-				echo '<form id="researchPage" action="../scripts/phpScripts/submitresearch.php" method="POST">';
-			}
-		 ?>
-			<div id="main">
-				<div id="intro_text">
-					<?php // This is the text title
-						if ($newPage == FALSE){
-							echo '<input name="Title" id="title" placeholder="Default Research Title" id="title" value="'.$research["Title"].'" type="text">';
-						} else {
-							echo '<input name="Title" id="title" placeholder="Default Research Title" id="title" value="" type="text">';
+		<div id="wrap_edit_r">
+			<div id="reject-message">
+			<?php
+				if ($newPage == FALSE ){// change to false
+					if($research["Verified"] == false){
+						if ($research["RejectMessage"] != ""){
+							echo 'Rejected. Reason:'.$research["RejectMessage"];
+						} else{
+							echo 'Not yet Approved/Rejected';
 						}
-					?>
-					<div id="editor-container">
-						<div id="editor" class="pell"></div>
-						<?php // this is the text body
-							if ($newPage == FALSE){
-								echo '<textarea hidden name="Body" id="markup">'.$research["Body"].'</textarea>';
-							} else {
-								echo '<textarea hidden name="Body" id="markup"></textarea>';
-							}
-						?>
-					</div>
-					
-				</div>
-				<div class="research_trio">
-					<div class="research">
-						<div>This is the blurb section!</div>
-						
-						<img id="image1" src="research_image1.jpg" alt="Research Image 1" /> <!-- DO THIS LATER, NO WAY TO UPLOAD IMAGES YET -->
-						<input type="text" id="image1-url" placeholder="THIS IS CURRENTLY A PLAEHOLDER THERE IS NOTHING HERE" />
-						<?php // this is the blurb
-							if ($newPage == FALSE){
-								echo '<textarea name="Blurb" rows="3" cols="40" placeholder="Please enter a blurb for your research, no more than 150 words as anything past that will not be displayed">'.$research["Blurb"].'</textarea>';
-							} else {
-								echo '<textarea name="Blurb" rows="3" cols="40" placeholder="Please enter a blurb for your research, no more than 150 words as anything past that will not be displayed"></textarea>';
-							}
-						?>
-					</div>
-				</div>
+					} else{
+						echo 'Approved!';
+					}
+				}
+			 ?>
 			</div>
-		</form>
-		<button form="researchPage" type="submit">Submit Research</button>
+			<?php
+				if ($newPage == FALSE){
+					echo '<form id="researchPage" action="../scripts/phpScripts/submitresearch.php?_id='.$_GET["_id"].'" method="POST">';
+				} else {
+					echo '<form id="researchPage" action="../scripts/phpScripts/submitresearch.php" method="POST">';
+				}
+			 ?>
+				<div id="main">
+					<div id="intro_text">
+						<?php // This is the text title
+							if ($newPage == FALSE){
+								echo '<input name="Title" id="title" placeholder="Default Research Title" id="title" value="'.$research["Title"].'" type="text">';
+							} else {
+								echo '<input name="Title" id="title" placeholder="Default Research Title" id="title" value="" type="text">';
+							}
+						?>
+						<div id="editor-container">
+							<div id="editor" class="pell"></div>
+							<?php // this is the text body
+								if ($newPage == FALSE){
+									echo '<textarea hidden name="Body" id="markup">'.$research["Body"].'</textarea>';
+								} else {
+									echo '<textarea hidden name="Body" id="markup"></textarea>';
+								}
+							?>
+						</div>
+			
+					</div>
+					<div class="research_trio">
+						<div class="research">
+							<div>This is the blurb section!</div>
+			
+							<img id="image1" src="research_image1.jpg" alt="Research Image 1" /> <!-- DO THIS LATER, NO WAY TO UPLOAD IMAGES YET -->
+							<input type="text" id="image1-url" placeholder="THIS IS CURRENTLY A PLAEHOLDER THERE IS NOTHING HERE" />
+							<?php // this is the blurb
+								if ($newPage == FALSE){
+									echo '<textarea name="Blurb" rows="3" cols="40" placeholder="Please enter a blurb for your research, no more than 150 words as anything past that will not be displayed">'.$research["Blurb"].'</textarea>';
+								} else {
+									echo '<textarea name="Blurb" rows="3" cols="40" placeholder="Please enter a blurb for your research, no more than 150 words as anything past that will not be displayed"></textarea>';
+								}
+							?>
+						</div>
+					</div>
+				</div>
+			</form>
+			<button form="researchPage" type="submit">Submit Research</button>
+		</div>
 		<?php genFooter();?>
 
 		<script src="https://unpkg.com/pell"></script>
