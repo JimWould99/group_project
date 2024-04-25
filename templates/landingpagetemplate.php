b7e79becedfc458065f11df08a4f8a2b58d03f7c
Gabriel Roberts <gabrielroberts2699@gmail.com>
2024-04-25 10:35:43 +0100

    removed div
    image div removed

--- templates/landingpagetemplate.php
+++ templates/landingpagetemplate.php
@@ -4,7 +4,6 @@
     function researchCard($rpd) {
         echo "<div id='research_card' onclick='location=\"researchpage.php?_id=".$rpd['_id']."\"'>";
         echo "<p id='title'>" . $rpd['Title'] . "</p>";
-        echo "<div id='image'></div>";
         if(isset($rpd['Images'][0])){echo "<img id='image' src=" . $rpd['Images'][0] . ">";}
         echo "<p id='short_bio'>" . $rpd["Blurb"] . "</p>";
         echo "<p id='author'>" . $rpd['Username'] . "</p>";// currently uses username, not author name
