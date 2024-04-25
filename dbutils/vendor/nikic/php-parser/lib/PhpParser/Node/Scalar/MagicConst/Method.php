ab129ddeb05e8294896d7f7dcd4377067ede2877
JimWould99 <130195351+JimWould99@users.noreply.github.com>
2024-04-25 13:30:41 +0100

    aa

--- dbutils/vendor/nikic/php-parser/lib/PhpParser/Node/Scalar/MagicConst/Method.php
+++ dbutils/vendor/nikic/php-parser/lib/PhpParser/Node/Scalar/MagicConst/Method.php
@@ -4,12 +4,12 @@ namespace PhpParser\Node\Scalar\MagicConst;
 
 use PhpParser\Node\Scalar\MagicConst;
 
-class Line extends MagicConst {
+class Method extends MagicConst {
     public function getName(): string {
-        return '__LINE__';
+        return '__METHOD__';
     }
 
     public function getType(): string {
-        return 'Scalar_MagicConst_Line';
+        return 'Scalar_MagicConst_Method';
     }
 }
