b11e9441a578e4fac93a1237d56a7626ee03737d
Gabriel Roberts <gabrielroberts2699@gmail.com>
2024-05-02 11:07:39 +0100

    further fixes to img
    Further fixes

--- templates/overviewtemplate.php
+++ templates/overviewtemplate.php
@@ -4,7 +4,7 @@
     function overviewCard($rpd) {
         echo "<div id='research_card' onchange='".$rpd['_id']."'>";
         echo "<p id='title'>" . $rpd['Title'] . "</p>";
-        if(isset($rpd['Images']["thumbnail1"])){echo "<img id='image' src=" . $rpd['Images']["thumbnail".$rpd['_id']] . ">";}
+        if(isset($rpd['Images']["thumbnail".$rpd['_id']])){echo "<img id='image' src=" . $rpd['Images']["thumbnail".$rpd['_id']] . ">";}
         echo "<p id='short_bio'>" . $rpd["Blurb"] . "</p>";
         echo "<p id='author'>" . $rpd['Username'] . "</p>";// currently uses username, not author name
         echo "</div>";
