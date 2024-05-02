32efaf5741b30e1db88dade2b5ae22d95fa3b71d
Gabriel Roberts <gabrielroberts2699@gmail.com>
2024-05-02 11:31:57 +0100

    MORE FIXES TO IMAGES

--- scripts/phpScripts/submitresearch.php
+++ scripts/phpScripts/submitresearch.php
@@ -9,8 +9,8 @@
 		$_id = createResearchPage($username)->getInsertedId(); //makes a new page and gets its id
 	}
 	
-	if(isset($_FILES['Thumbnail'])) {
-		storeResearchImage($_FILES['Thumbnail'], getResearchPage($_id), $_id);
+	if(isset($_FILES['Thumbnail']) && $_FILES["Thumbnail"]["error"] == 0 ) {
+		storeResearchImage($_FILES['Thumbnail'], getResearchPage($_id), $_id); //SOMETHIN IN HERE BREAKS WHEN FILE UPLOADED
 	}
 
 	if(isset($_FILES['uploadFile'])) {
